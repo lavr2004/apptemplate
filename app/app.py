e42938692659f22af5ef219aed051b1019cf1c9e
lavr2004 <lavr2004@gmail.com>
2019-07-23 18:42:51 +0200

    refactoring

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
 from .bin.database import Database_mysql
-from .bin.settings import Settings
+from .bin.settings import *
 
 class App():
-    database_obj = Database_mysql()
+    database_obj = Database_mysql(LOGIN_str, PASSWORD_str, HOST_str, SCHEMA_str)
     settings_obj = Settings()
 
     def process_cf(self):
